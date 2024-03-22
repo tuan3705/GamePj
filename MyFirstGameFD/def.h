8185a4d532b5fe85ba5a22383b9b03a841a97d39
tuan3705 <dtuanat3705@gmail.com>
2024-03-23 02:37:43 +0700

    game

---
+++ MyFirstGameFD/def.h
@@ -0,0 +1,8 @@
+#ifndef _DEF__H
+#define _DEF__H
+
+const int SCREEN_WIDTH = 350;
+const int SCREEN_HEIGHT = 485;
+const char* WINDOW_TITLE = "Flappy Doge";
+#endif
+
--- MyFirstGameFD/main.cpp
+++ MyFirstGameFD/main.cpp
@@ -1,9 +1,39 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#include "def.h"
+#include "graphics.h"
 using namespace std;
 
-int main()
+
+void waitUntilKeyPressed()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    SDL_Event e;
+    while (true) {
+        if ( SDL_PollEvent(&e) != 0 &&
+             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
+            return;
+        SDL_Delay(100);
+    }
+
+}
+int main(int argc, char* argv[])
+{
+     Graphics graphics;
+     graphics.init();
+
+     SDL_Texture* background = graphics.loadTexture("Picture/background.png");
+     graphics.prepareScene(background);
+    // graphics.presentScene();
+     SDL_Texture* shiba = graphics.loadTexture("Picture/shiba.png");
+     graphics.renderTexture(shiba, 150, 200);
+	 graphics.presentScene();
+    // waitUntilKeyPressed();
+     waitUntilKeyPressed();
+
+
+
+
+     waitUntilKeyPressed();
+     graphics.quit();
+ //    quitSDL(window, renderer);
+
 }
