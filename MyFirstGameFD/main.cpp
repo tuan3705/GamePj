a2c1381af19956e4d95206ce0893423ec5f22682
tuan3705 <dtuanat3705@gmail.com>
2024-04-17 04:51:52 +0700

    clean file

--- MyFirstGameFD/main.cpp
+++ MyFirstGameFD/main.cpp
@@ -1,13 +1,8 @@
 #include<bits/stdc++.h>
-#include "gameloop.h"
 #include "game.h"
 using namespace std;
 int main(int argc, char *argv[])
 {
-//   srand(time(0));
-//   Graphics graphics;
-//   graphics.init();
-//   playGame(graphics);
    Game game;
    game.initG();
    while(!game.isQuit())
